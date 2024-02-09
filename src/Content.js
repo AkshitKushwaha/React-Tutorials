73450f37a43ff4964e2ed05391307309e92933d8
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2024-02-09 19:44:18 +0530

    Added check functionality

--- src/Content.js
+++ src/Content.js
@@ -1,76 +1,66 @@
-import React from 'react'
-import {useState} from 'react'
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
-export default function Content() {
+const Content = () => {
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: true,
+            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
+    ]);
 
-  /*
-
-  const [name, setName] = useState('Akshit');
-  const [count, setCount] = useState(0);
-
-  const handleNameChange = ()=> {
-    const names = ['A', 'B', 'C'];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
-  }
-
-  const handleClick = ()=>{
-    setCount(count + 1);
-    console.log(count)
-  }
-
-  
-  const handleClick2 = (name)=>{
-    console.log(`${name} was clicked`)
-  }
-
-  const handleClick3 = (e)=>{
-    console.log(e)
-  }
-
-      <main>
-
-  
-        <p onDoubleClick={handleClick}>
-          Hello {name};
-        </p>
-
-        <button onClick={handleNameChange}>Change Name</button>
-        <button onClick={handleClick}>Click me 2</button>
-        <button onClick={(e)=> handleClick3(e)}>Click me 3</button>
-
-    </main>
-
-  */
-
-const Content = ()=> {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2"
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3"
+    const handleCheck = (id) => {
+        //console.log(`key: ${id}`)
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
     }
-    
-  ]);
 
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
+    return (
+        <main>
+            {items.length ? (
+                <ul>
+                    {items.map((item) => (
+                        <li className="item" key={item.id}>
+                            <input
+                                type="checkbox"
+                                onChange={() => handleCheck(item.id)}
+                                checked={item.checked}
+                            />
+                            <label
+                                style={(item.checked) ? { textDecoration: 'line-through' } : null}
+                                onDoubleClick={() => handleCheck(item.id)}
+                            >{item.item}</label>
+                            <FaTrashAlt
+                                onClick={() => handleDelete(item.id)}
+                                role="button"
+                                tabIndex="0"
+                            />
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+            )}
+        </main>
+    )
 }
 
-  return (
-    <main>
-
-    </main>
-
-  )
-}
-
-//()=> is the anonymous function
+export default Content
