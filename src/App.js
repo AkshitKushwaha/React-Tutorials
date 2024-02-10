8f78d495c872e3b9e28de226ba7cf60378424829
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2024-02-10 19:18:39 +0530

    added a functionality to add items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import AddItem from './AddItem';
 import Content from'./Content';
 import Footer from './Footer';
 import { useState } from 'react';
@@ -6,40 +7,48 @@ import { useState } from 'react';
 
   function App() {
 
-    const [items, setItems] = useState([
-      {
-          id: 1,
-          checked: true,
-          item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-      },
-      {
-          id: 2,
-          checked: false,
-          item: "Item 2"
-      },
-      {
-          id: 3,
-          checked: false,
-          item: "Item 3"
-      }
-    ]);
+    const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+    
+    const [newItem, setNewItem] = useState('');
+
+    const setAndSaveItems = (newItems)=> {
+      setItems(newItems);
+      localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+    }
+
+    const addItem = (item)=> {
+      const id = items.length ? items[items.length - 1].id + 1 : 1;
+      const myNewItem = {id, checked: false, item};
+      const listItems = [...items, myNewItem];
+      setAndSaveItems(listItems);
+    }
     
     const handleCheck = (id) => {
     //console.log(`key: ${id}`)
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
     }
     
     const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+    }
+
+    const handleSubmit = (e)=> {
+      e.preventDefault();
+      if (!newItem) return;
+      addItem(newItem);
+      setNewItem('');
     }
     
   return (
     <div className="App">
       <Header title="Grocery List"/>
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content items={items}
                handleCheck={handleCheck}
                handleDelete={handleDelete}
@@ -50,3 +59,5 @@ import { useState } from 'react';
 }
 
 export default App;
+
+//e means event object
