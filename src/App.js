8cf259278b1a9be0b2aab63cc921dc3a5c1452d0
Akshit Kushwaha <id.akshitkushwaha@gmail.com>
2024-02-26 09:27:47 +0530

    Improved

--- src/App.js
+++ src/App.js
@@ -8,15 +8,38 @@ import SearchItem from './SearchItem';
 
   function App() {
 
-    const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+    const API_URL = 'http://localhost:3500/items';
+
+    const [items, setItems] = useState([]);
     
     const [newItem, setNewItem] = useState('');
 
     const [search, setSearch] = useState('');
 
+    const [fetchError, setFetchError] = useState(null);
+
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
-      localStorage.setItem('shoppinglist', JSON.stringify(items));
-    }, [items])
+      
+      const fetchItems = async ()=> {
+        try {
+          const response = await fetch(API_URL);
+          if(!response.ok) throw Error('Failed to fetch data');
+          const listItems = await response.json();
+          setItems(listItems);
+          setFetchError(null);
+        } catch (err) {
+          setFetchError(err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      setTimeout(()=> {
+        (async ()=> await fetchItems())();
+      }, 1000);
+    }, [])
 
     const addItem = (item)=> {
       const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -59,10 +82,14 @@ import SearchItem from './SearchItem';
         setSearch={setSearch}
       />
 
-      <Content items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-               handleCheck={handleCheck}
-               handleDelete={handleDelete}
-      />
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{color: "red"}}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && <Content items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+                handleCheck={handleCheck}
+                handleDelete={handleDelete}
+        />}
+      </main>
 
       <Footer length={items.length} />
 
